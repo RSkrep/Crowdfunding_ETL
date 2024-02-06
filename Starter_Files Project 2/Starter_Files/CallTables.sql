49ce06b79349455e5c3dbf27cf953cdbbf6bc5c2
RSkrep <148006370+RSkrep@users.noreply.github.com>
2024-02-05 19:25:18 -0500

    Add files via upload

---
+++ Starter_Files Project 2/Starter_Files/CallTables.sql
@@ -0,0 +1,4 @@
+SELECT * FROM crowdfunding_db.PUBLIC."Campaigns";
+SELECT * FROM crowdfunding_db.PUBLIC."Category";
+SELECT * FROM crowdfunding_db.PUBLIC."Contacts";
+SELECT * FROM crowdfunding_db.PUBLIC."Subcategory";
